00adfcaf04536068703153fefde52c9205b4afbe
Kenet Picado <kenetpicado1@gmail.com>
2024-03-16 12:05:46 -0600

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tracker/',
+  base: 'https://rastrear.enviosdeoccidente.com/',
   plugins: [vue()],
   resolve: {
     alias: {
