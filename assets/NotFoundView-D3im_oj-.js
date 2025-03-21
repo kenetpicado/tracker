75ab99b7162da1155e551b527fb043be55aded82
Kenet <kenetpicado1@gmail.com>
2025-03-20 21:15:19 -0600

    deploy

--- assets/NotFoundView-D3im_oj-.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as s,a as c}from"./index-DtWSueiT.js";const o={},n={class:"min-h-screen flex items-center justify-center uppercase text-gray-400"},r=c("h1",null,"Not Found",-1),a=[r];function _(i,d){return s(),t("div",n,a)}const l=e(o,[["render",_]]);export{l as default};
---
+++ assets/NotFoundView-DGXQfXHk.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s,a as c}from"./index-B9TVh-ls.js";const o={},n={class:"min-h-screen flex items-center justify-center uppercase text-gray-400"},r=c("h1",null,"Not Found",-1),a=[r];function _(i,d){return s(),t("div",n,a)}const l=e(o,[["render",_]]);export{l as default};
